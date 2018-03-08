4fb5547f87dbc684054ec3686d01b928782ee751
Isaac Gifford <isaac@geekingreen.com>
2018-03-08 10:32:13 -0700

    refactor SW

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ let babelify = require('babelify');
 let source = require('vinyl-source-stream');
 let buffer = require('vinyl-buffer');
 let path = require('path');
+let es = require('event-stream');
 
 const paths = {
   phaser: './node_modules/phaser/dist/',
@@ -31,8 +32,18 @@ const paths = {
   game: {
     entry: './src/js/Game.js',
     dest: 'game.js'
+  },
+  serviceWorker: {
+    entry: './src/js/sw.js',
+    dest: 'sw.js',
+    destPath: './build'
   }
 };
+
+const scriptPaths = [
+  paths.game,
+  paths.serviceWorker
+];
 
 gulp.task('default', ['serve']);
 
@@ -40,7 +51,7 @@ gulp.task('serve', ['build-dev'], function() {
   gulp.watch(paths.styles.src + '/**/*', ['styles']);
   gulp.watch(paths.assets.src + '/**/*', ['copy-assets']);
   gulp.watch(paths.script.src + '/**/*.js', ['lint', 'scripts']);
-  gulp.watch(paths.base + '/*.{html,js,json}', ['copy-html']);
+  gulp.watch(paths.base + '/index.html', ['copy-html']);
 
   gulp.watch(paths.build + '/**/*').on('change', browserSync.reload);
 
@@ -60,7 +71,7 @@ gulp.task('clean', function() {
 gulp.task('copy-static', ['copy-html', 'copy-assets', 'copy-phaser']);
 
 gulp.task('copy-html', function() {
-  gulp.src([paths.base + '/*.{html,js,json}'])
+  gulp.src([paths.base + '/index.html', paths.base + '/manifest.json'])
     .pipe(gulp.dest(paths.build));
 });
 
@@ -94,37 +105,45 @@ gulp.task('lint', function () {
 });
 
 gulp.task('scripts', function() {
-  return browserify(
-    {
-      paths: [path.join(__dirname, paths.script.src)],
-      entries: paths.game.entry,
-      debug: true
-    })
-    .transform(babelify, {
-      babel: require('@babel/core'),
-      sourceMaps: true
-    })
-    .bundle()
-    .pipe(source(paths.game.dest))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./srcmaps'))
-    .pipe(gulp.dest(paths.script.dest));
+  const tasks = scriptPaths.map(p => {
+    return browserify(
+      {
+        paths: [path.join(__dirname, paths.script.src)],
+        entries: p.entry,
+        debug: true
+      })
+      .transform(babelify, {
+        babel: require('@babel/core'),
+        sourceMaps: true
+      })
+      .bundle()
+      .pipe(source(p.dest))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write('./srcmaps'))
+      .pipe(gulp.dest(p.destPath || paths.script.dest));
+  });
+
+  return es.merge.apply(null, tasks);
 });
 
 gulp.task('dist', function() {
-  return browserify(
-    {
-      paths: [path.join(__dirname, paths.script.src)],
-      entries: paths.game.entry,
-      debug: false
-    })
-    .transform(babelify, {
-      babel: require('@babel/core'),
-    })
-    .bundle()
-    .pipe(source(paths.game.dest))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.script.dest));
+  const tasks = scriptPaths.map(p => {
+    return browserify(
+      {
+        paths: [path.join(__dirname, paths.script.src)],
+        entries: p.entry,
+        debug: false
+      })
+      .transform(babelify, {
+        babel: require('@babel/core'),
+      })
+      .bundle()
+      .pipe(source(p.dest))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(gulp.dest(p.destPath || paths.script.dest));
+  });
+
+  return es.merge.apply(null, tasks);
 });
