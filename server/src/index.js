668310e6b50980f23d61eb1c31b530ff9c81c83b
Ignacio X. Domínguez <ignacioxd@ncsu.edu>
2018-03-12 22:49:34 -0400

    Begin player socket abstraction

--- server/src/index.js
+++ server/src/index.js
@@ -3,14 +3,20 @@ import express from 'express';
 import http from 'http';
 import io from 'socket.io';
 
+import PlayerManager from './game/PlayerManager';
+
 let app = express();
 let server = http.Server(app);
 let socketio = io(http);
 
+let playerManager = new PlayerManager();
+
 socketio.on('connection', function(socket) {
+  playerManager.playerConnected(socket);
   console.log('a user connected');
   socket.on('disconnect', function() {
     console.log('user disconnected');
+    playerManager.playerDisconnected(socket);
   });
 });
 
