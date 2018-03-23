fa6fed2723f72b07067b2d56c820c533fee50fe1
Pepper <jenearly@gmail.com>
2018-03-23 13:55:41 -0400

    the rest of the buttons

--- game/src/js/objects/ui/Checkbox.js
+++ game/src/js/objects/ui/Checkbox.js
@@ -1,10 +1,10 @@
 export default class Checkbox extends Phaser.GameObjects.Group {
   constructor(scene, x, y, text, defaultChecked) {
-    const scale = 0.3;
-    const fontSize = 66 * scale;
+  // const scale = 0.3;
+    const fontSize = 66;
 
-    let tickbox = scene.add.image(x, y, 'checkbox', defaultChecked ? 'checked' : 'unchecked');
-    tickbox.setScale(scale);
+    let tickbox = scene.add.image(x, y, 'checkbox', defaultChecked ? 'checked.png' : 'unchecked.png');
+    // tickbox.setScale(scale);
     let label = scene.add.text(x + tickbox.displayWidth - 15, y - fontSize / 2, text, {
       font: `${fontSize}px Arial`,
       fill: '#ffffff'
@@ -36,7 +36,7 @@ export default class Checkbox extends Phaser.GameObjects.Group {
 
   setChecked(checked) {
     this.checked = checked == true;
-    this.tickbox.setFrame(this.checked ? 'checked' : 'unchecked');
+    this.tickbox.setFrame(this.checked ? 'checked.png' : 'unchecked.png');
   }
 
   onPointerDown(handler) {
--- game/src/js/scenes/TitleScene.js
+++ game/src/js/scenes/TitleScene.js
@@ -24,38 +24,37 @@ export default class TitleScene extends Phaser.Scene {
     // });
 
     let logo = this.add.image(400, 100, 'logo');
+    //multi player button
    
-
-    // emitter.startFollow(logo);
-    let single_player_button = new Button(this, 280, 440, 'single_player');
-   
-    single_player_button.buttonDown(button => 
-    {
-      this.scene.start('CharacterSelectionScene', {type: 'single_player'});
-    })
-
-
-    let multi_player_button = new Button(this, 280, 300, 'multi_player');
+    let multi_player_button = new Button(this, 280, 200, 'multi_player');
    
     multi_player_button.buttonDown(button => 
     {
       this.scene.start('CharacterSelectionScene', {type: 'multi_player'});
     })
 
-    // this.add.text(220, 350, 'Press ENTER to begin', {
-    //   font: '34px Arial',
-    //   fill: '#ffffff',
-    //   stroke: '#000000',
-    //   strokeThickness: '6',
-    //   shadowFill: '#ffffff',
-    //   shadowStroke: '#ffffff',
-    //   shadowOffsetY: '10',
-    //   shadowBlur: '5'
-    // });
+    // emitter.startFollow(logo);
+    //single player button
+    let single_player_button = new Button(this, 280, 320, 'single_player');
+   
+    single_player_button.buttonDown(button => 
+    {
+      this.scene.start('CharacterSelectionScene', {type: 'single_player'});
+    })
+
+    const text_style = {font: "bold 18px Arial", fill: "#fff", boundsAlignH: "center", backgroundColor: 'rgba(0,0,0)', boundsAlignV: "middle"};
+    
+    //controls, credits, offline mode buttons + their texts
+    let controls_button = new Button(this, 150, 433, 'controls_button');
+    let controls = this.add.text(108, 490, 'controls', text_style);
+    let credits_button = new Button(this, 275, 433, 'credits_button');
+    let credits = this.add.text(235, 490, 'credits', text_style);
+    let SW = this.add.text(355, 490, 'enable offline mode', text_style)
+  
 
     let serviceWorker = new ServiceWorker();
     
-    let checkbox = new Checkbox(this, 145, 560, 'Enable offline mode', serviceWorker.isRegistered());
+    let checkbox = new Checkbox(this, 400, 433, serviceWorker.isRegistered());
 
     checkbox.onPointerDown(function(obj) {
       //TODO: add service worker
