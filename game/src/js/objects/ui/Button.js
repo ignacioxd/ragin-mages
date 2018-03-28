8cea6facc7199b94ddc6eaf2113196d85f85bfd7
Ignacio X. Domínguez <ignacioxd@ncsu.edu>
2018-03-28 14:01:49 -0400

    Allow button properties to be customizable

--- game/src/js/objects/ui/Button.js
+++ game/src/js/objects/ui/Button.js
@@ -1,24 +1,28 @@
 export default class Button extends Phaser.GameObjects.Text {
-  constructor(scene, x, y, text, disabled = false) {
+  constructor(scene, x, y, text, options = {}) {
     super(scene, x, y, text, {
       fontSize: 30,
       fontFamily: "'Fjalla One', sans-serif",
-      fill: '#d3d3d3',
-      width: '500px',
-
+      fill: '#d3d3d3'
     });
-    let padding = {left: 5, right: 400 - this.width, top: 0, bottom: 0};
-    this.disabled = disabled;
+
+    this.props = {
+      width: 400,
+      disabled: false,
+      ...options
+    };
+
+    let padding = {left: 5, right: this.props.width - this.width, top: 0, bottom: 0};
 
     this.setStroke('#000000', 6);
     this.setShadow(0, 0, '#000000', 4, false, false);
     this.setBackgroundColor('#ffffff00').setPadding(padding);
     this.setInteractive();
 
-    if (this.disabled) this.setFill('#333333');
+    this.setDisabled(this.props.disabled);
 
     this.on('pointerover', () => {
-      if (!this.disabled) {
+      if (!this.props.disabled) {
         this.setFill('#ffffff');
         this.setBackgroundColor('#ffffff55').setPadding(padding);
         
@@ -28,7 +32,7 @@ export default class Button extends Phaser.GameObjects.Text {
     });
 
     this.on('pointerout', () => {
-      if (!this.disabled) {
+      if (!this.props.disabled) {
         this.setFill('#d3d3d3');
         this.setBackgroundColor('#ffffff00').setPadding(padding);
         this.setStroke('#000000', 6);
@@ -41,13 +45,13 @@ export default class Button extends Phaser.GameObjects.Text {
 
   buttonDown(handler) {
     this.on('pointerdown', () => {
-      if (!this.disabled) handler(this);
+      if (!this.props.disabled) handler(this);
     })
   }
 
   setDisabled(value) {
-    this.disabled = value;
-    if (this.disabled) this.setFill('#333333');
+    this.props.disabled = value;
+    if (this.props.disabled) this.setFill('#333333');
     else this.setFill('#d3d3d3');
   }
   
