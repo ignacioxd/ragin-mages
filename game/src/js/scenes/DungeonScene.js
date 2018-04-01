aec92fa138226710d6df9024e7add487d2967e06
Adnan Usman <adnantu@hotmail.com>
2018-04-01 10:06:57 -0700

    Made the single player run without sockets

--- game/src/js/scenes/DungeonScene.js
+++ game/src/js/scenes/DungeonScene.js
@@ -1,15 +1,12 @@
 import BaseScene from './BaseScene';
 import Controller from '../util/Controller';
 import Character from 'objects/Character';
-import DOMModal from 'objects/ui/DOMModal';
 
 export default class DungeonScene extends BaseScene {
 
   constructor() {
     super({ key: 'DungeonScene' });
-    this.clientId = null;
 
-    this.players = new Map();
     this.localCharacter = null;
   }
 
@@ -22,7 +19,7 @@ export default class DungeonScene extends BaseScene {
     //Create collision groups and event handling
     this.projectiles = this.add.group();
     this.characters = this.add.group();
-    this.physics.add.overlap(this.projectiles, this.characters, this.playerHit, null, this);
+    this.physics.add.overlap(this.projectiles, this.characters, null, this);
 
 
     this.controller = new Controller(this);
@@ -49,7 +46,6 @@ export default class DungeonScene extends BaseScene {
         let worldX = event.x + event.camera.scrollX * event.camera.zoom;
         let worldY = event.y + event.camera.scrollY * event.camera.zoom;
         this.localCharacter.fire(worldX, worldY, this.clientId);
-        this.socket.emit('fire', this.localCharacter.x, this.localCharacter.y, worldX, worldY);
       }
     }, this);
 
@@ -61,20 +57,7 @@ export default class DungeonScene extends BaseScene {
   }
 
   create() {
-    //load config file for socket information
-    let serverConfig = this.cache.json.get('config');
-    this.socket = io(`${serverConfig.protocol}://${serverConfig.host}:${serverConfig.ioport}`);
-
-    this.socket.on('connect', this.serverConnected.bind(this));
-    this.socket.on('setId', this.setId.bind(this));
-    this.socket.on('existingPlayers', this.existingPlayers.bind(this));
-    this.socket.on('spawn', this.spawn.bind(this));
-    this.socket.on('playerJoined', this.playerJoined.bind(this));
-    this.socket.on('playerLeft', this.playerLeft.bind(this));
-    this.socket.on('playerMoved', this.playerMoved.bind(this));
-    this.socket.on('playerFired', this.playerFired.bind(this));
-    this.socket.on('playerDied', this.playerDied.bind(this));
-    this.socket.on('playerDisconnected', this.playerDisconnected.bind(this));
+    this.spawn(0,0);
 
   }
 
@@ -82,125 +65,12 @@ export default class DungeonScene extends BaseScene {
     if(this.localCharacter) {
       const vector = this.controller.getWASDVector();
       this.localCharacter.setMotion(vector);
-      if(this.localCharacter.shouldBroadcastMotion()) {
-        console.log('motion changed locally');
-        this.socket.emit('move', this.localCharacter.x, this.localCharacter.y, vector.x, vector.y);
-      }
     }
-  }
-
-  playerHit(projectile, character) {
-    projectile.destroy();
-    if(character.hit(projectile)) { //If the hit causes the player to die
-      this.socket.emit('die', character.x, character.y, projectile.props.owner.id);
-      new DOMModal('killed', {
-        acceptButtonSelector: '#respawn',
-        cancelButtonSelector: '.exit',
-        onAccept: (modal) => {
-          modal.close();
-          this.socket.emit('respawn');
-        },
-        onCancel: (modal) => {
-          modal.close();
-          this.socket.emit('leaveGame');
-          this.socket.disconnect();
-          this.scene.start('TitleScene');
-        },
-        data: character.stats
-      });
-      this.localCharacter = null;
-    }
-  }
-  //WebSocket Messages
-  serverConnected() {
-    console.log('serverConnected');
-  }
-
-  setId(id) {
-    this.clientId = id;
-    this.socket.emit('joinGame', this.characterType, '');
-  }
-
-  existingPlayers(existingPlayers) {
-    console.log('existingPlayers');
-    console.log(existingPlayers);
-    existingPlayers.forEach(value => {
-      this.playerJoined(value.id, value.character, value.handle, value.x, value.y);
-    });
   }
 
   spawn(x, y) {
     this.localCharacter = new Character(this, x, y, this.characterType);
     this.characters.add(this.localCharacter); //this is us.
     this.cameras.main.startFollow(this.localCharacter);
-  }
-
-  playerJoined(id, character, handle, x, y) {
-    character = character == 'priest' ? 'priest_hero' : character; //Temp fix for compatibility with old clients
-    console.log('playerJoined');
-    if(this.clientId !== id) {
-      let remotePlayer = new Character(this, x, y, character);
-      this.players.set(id, remotePlayer);
-      remotePlayer.id = id;
-      //remotePlayer.setHandle(handle);
-    }
-    else {
-      console.log('this should never happen!!!!!');
-    }
-  }
-
-  playerLeft(id) {
-    console.log('playerLeft');
-    let player = this.players.get(id);
-    if(!player) return;
-    player.die();
-  }
-
-  playerMoved(id, x, y, vecX, vecY) {
-    console.log('playerMoved');
-    let player = this.players.get(id);
-    if(!player) return;
-    this.tweens.killTweensOf(player);
-    this.tweens.add({
-      targets: player,
-      x: x,
-      y: y,
-      duration: 50,
-      ease: 'Linear'
-    });
-
-    //player.setPosition(x, y);
-    player.setMotion(new Phaser.Math.Vector2(vecX, vecY), false);
-  }
-
-  playerFired(id, fromX, fromY, toX, toY) {
-    console.log('playerFired');
-    let player = this.players.get(id);
-    if(!player) return;
-    player.setPosition(fromX, fromY);
-    let projectile = player.fire(toX, toY);
-    this.projectiles.add(projectile);
-  }
-
-  playerDied(id, x, y, killedById) {
-    if(killedById == this.clientId) {
-      this.localCharacter.stats.kills++; 
-    }
-    console.log('playerDied');
-    let player = this.players.get(id);
-    if(!player) return;
-    this.tweens.killTweensOf(player);
-    player.setPosition(x, y);
-    player.die();
-  }
-
-  playerDisconnected(id) {
-    this.tweens.killTweensOf(player);
-    let player = this.players.get(id);
-    if(!player) return;
-    this.tweens.killTweensOf(player);
-    this.characters.remove(player);
-    this.players.delete(id);
-    player.die();
   }
 }
