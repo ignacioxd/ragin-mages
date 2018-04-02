5da67e1a59a1461dc75ff17cadb8a91a3ed970cf
Doug <35356296+Miku1338@users.noreply.github.com>
2018-04-02 09:24:30 -0700

    Fixed so that enemies added to the character list
    Enemies should now have collision detection (assuming collision detection works).

--- game/src/js/scenes/DungeonScene.js
+++ game/src/js/scenes/DungeonScene.js
@@ -77,7 +77,8 @@ export default class DungeonScene extends BaseScene {
     if (this.delay === 0) {
       const newMonster = new SpiderMonster(this, 250 * (Math.random() - 0.5), 250 * (Math.random() - 0.5));
       newMonster.setAIOn(this.localCharacter);
-      this.enemyList.push(newMonster);
+      this.enemyList.add(newMonster);
+      this.characters.add(newMonster);
       this.delay = 30;
     }
     
